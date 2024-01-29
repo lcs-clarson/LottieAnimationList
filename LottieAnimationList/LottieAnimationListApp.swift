2aae982ed42b62c86559727deb12bce07c0b677c
Charlie Larson <clarson@lcs.on.ca>
2024-01-29 09:58:15 -0500

    Initial Commit

---
+++ LottieAnimationList/LottieAnimationListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationListApp.swift
+//  LottieAnimationList
+//
+//  Created by Charlie Larson on 2024-01-29.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
